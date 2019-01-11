4e4c090c079d6aee01ebdea5f36291d5f3908046
KillerBee1O1 <sudiptdabral2991999@gmail.com>
2019-01-12 02:39:19 +0530

    added funtionality only designing left

--- public/js/index.js
+++ public/js/index.js
@@ -25,7 +25,36 @@ function sendMessage() {
 /* Function for receiving message */
 socket.on('receivedMessage', function (data) {
     console.log("RECEIVED MESSAGE", data);
+    var div = document.createElement('div');
+    div.setAttribute('class', 'message');
+    var h3 = document.createElement('h3');
+    h3.setAttribute('class', 'from');
+    h3.innerHTML = data.from;
+    div.appendChild(h3);
+    var span = document.createElement('span');
+    span.innerHTML = data.time;
+    span.setAttribute('class', 'time');
+    div.appendChild(span);
+    var p = document.createElement('p');
+    p.setAttribute('class', 'message-received');
+    p.innerHTML = data.text;
+    div.appendChild(p);
+    var messagePanel = document.getElementById('message-panel');
+    messagePanel.appendChild(div);
 })
+
+/* Funtion for receiving list of people */
+socket.on('updatedUserList', function (data) {
+    console.log("user list", data);
+    var peopleList = document.getElementById('people-list');
+    peopleList.innerHTML = "";
+    for (var i in data) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(`${data[i].name}`));
+        peopleList.appendChild(li);
+    }
+})
+
 
 /* deparams function to convert it into a object of key value */
 function QueryStringToJSON() {
